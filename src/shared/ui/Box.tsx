c5ad5840255d3f773d2e7535e52cce399ba23fbc
heesu0303 <heesu0303@naver.com>
2025-02-15 02:01:20 +0900

    refactor: shared/ui forwardRef 로 수정

--- src/shared/ui/Box.tsx
+++ src/shared/ui/Box.tsx
@@ -2,16 +2,14 @@ import React from 'react'
 
 import { cn } from '@/shared/lib/utils'
 
-interface BoxProps extends React.HTMLAttributes<HTMLDivElement> {
-    children: React.ReactNode
-}
-
-const Box: React.FC<BoxProps> = ({ children, className = '', ...props }) => {
-    return (
-        <div className={cn('flex items-center justify-center gap-2 p-4 my-0 mx-auto', className)} {...props}>
+const Box = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
+    ({ children, className, ...props }, ref) => (
+        <div ref={ref} className={cn('flex items-center justify-center gap-2 p-4 my-0 mx-auto', className)} {...props}>
             {children}
         </div>
-    )
-}
+    ),
+)
+
+Box.displayName = 'Box'
 
 export { Box }
