01bbd8ce2f6422950f6d8b8b919ef748d12e1175
owni14 <kmwoo514@gmail.com>
2025-02-26 02:30:58 +0900

    cicd: edit deploy logic

--- src/shared/ui/CustomEditor.tsx
+++ src/shared/ui/CustomEditor.tsx
@@ -1,21 +1,19 @@
-import { Editor } from '@toast-ui/react-editor'
-import React from 'react'
+// import { Editor } from '@toast-ui/react-editor'
 
-import { cn } from '@/shared/lib/utils'
-
-import '@toast-ui/editor/dist/toastui-editor.css'
-import '@toast-ui/editor/dist/theme/toastui-editor-dark.css'
+// import '@toast-ui/editor/dist/theme/toastui-editor-dark.css'
+// import '@toast-ui/editor/dist/toastui-editor.css'
 
 const CustomEditor = () => {
     return (
-        <Editor
-            initialEditType="wysiwyg"
-            // initialValue="hello react editor world!"
-            // previewStyle="vertical"
-            // height="600px"
-            // initialEditType="markdown"
-            // useCommandShortcut={true}
-        />
+        <></>
+        // <Editor
+        // initialEditType="wysiwyg"
+        // initialValue="hello react editor world!"
+        // previewStyle="vertical"
+        // height="600px"
+        // initialEditType="markdown"
+        // useCommandShortcut={true}
+        // />
     )
 }
 
