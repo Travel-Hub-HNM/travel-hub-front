47bcd06616fc958b997114c0e336c0c87ddd27d2
owni14 <kmwoo514@gmail.com>
2025-03-13 09:25:00 +0900

    fix: 타입 수정

--- src/shared/providers/MapProvider/index.tsx
+++ src/shared/providers/MapProvider/index.tsx
@@ -1,13 +1,15 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 'use client'
 
 import { LoadScriptNext } from '@react-google-maps/api'
+import { JSXElementConstructor, ReactElement } from 'react'
 
 import { DefaultValue } from '@/shared/config/consts'
 
 const GOOGLE_MAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || DefaultValue.BLANK
 
 interface MapProviderProps {
-    children: React.ReactElement
+    children: ReactElement<unknown, string | JSXElementConstructor<any>>
 }
 
 export function MapProvider({ children }: MapProviderProps) {
