fc52b2d59ff2ec219ed50400700412c4850a0e2c
owni14 <kmwoo514@gmail.com>
2025-04-08 09:12:38 +0900

    fix: instance 1개로 고정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
             env: {
                 NODE_ENV: 'production',
             },
-            instances: 'max',
+            instances: 1,
             exec_mode: 'cluster',
             max_memory_restart: '1G',
             autorestart: true,
